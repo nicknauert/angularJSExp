01f488632fa69b275fa73fff1bde58c02997e368
Nick Nauert <nicknauert@gmail.com>
2018-01-02 10:59:07 -0500

    routes issue patched

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,5 +1,5 @@
 angular.module('mainController', ['mainService'])
-  .controller('mainCtrl', function(mainSvc){
+  .controller('mainCtrl', function(mainSvc, $rootElement){
     const vm = this;
     this.toggleThing = function(){
       console.log($scope);
